8c349c50d1b384b67119caf388c9588c5a39da53
dpilafian <dem@centerkey.com>
2022-09-22 10:52:49 -0700

    Release v0.1.2

--- dist/copy-folder.umd.cjs
+++ dist/copy-folder.umd.cjs
@@ -1,4 +1,4 @@
-//! copy-folder-cli v0.1.1 ~~ https://github.com/center-key/copy-folder-cli ~~ MIT License
+//! copy-folder-cli v0.1.2 ~~ https://github.com/center-key/copy-folder-cli ~~ MIT License
 
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
