df6b3ae627cbb1469b455e818108f007226835f8
dpilafian <dem@centerkey.com>
2024-01-02 15:32:31 -0800

    Release v1.1.3

--- dist/copy-folder.js
+++ dist/copy-folder.js
@@ -1,4 +1,4 @@
-//! copy-folder-util v1.1.2 ~~ https://github.com/center-key/copy-folder-util ~~ MIT License
+//! copy-folder-util v1.1.3 ~~ https://github.com/center-key/copy-folder-util ~~ MIT License
 
 import chalk from 'chalk';
 import fs from 'fs';
